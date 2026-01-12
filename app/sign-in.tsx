0cadd133a9574dbe1b414defb02b274792ff280b
edenWerke <edenw6292@gmail.com>
2026-01-12 15:39:15 +0300

    sign in"

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,16 @@
+import images from '@/constants/images'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+<ScrollView contentContainerClassName='h-full'>
+
+  <Image source={images.onboarding} className='w-full h-4/6'  resizeMode='contain'></Image>
+</ScrollView>
+    </SafeAreaView>
   )
 }
 
