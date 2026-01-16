63c776a66f0665e07852bd1f31374230ebc77dbb
edenWerke <edenw6292@gmail.com>
2026-01-16 15:46:48 +0300

    summeray

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,31 @@
+import icons from '@/constants/icons'
+import images from '@/constants/images'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, ScrollView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
+  const handleLogout=async()=>{}
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className='h-full bg-white'>
+     <ScrollView showsVerticalScrollIndicator={false}
+     contentContainerClassName='pb-32 px-7'
+     >
+      <View className='flex flex-row items-center justify-between mt-5'>
+<Text className='text-xl font-rubik-bold'>Profile</Text>
+<Image source={icons.bell} className='size-5'/>
+
+
+      </View>
+     
+      <View className='flex-row justify-center flex mt-5'>
+        <View className='flex flex-col items-center relative mt-5'>
+
+          <Image source={images.avatar}/>
+        </View>
+      </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
