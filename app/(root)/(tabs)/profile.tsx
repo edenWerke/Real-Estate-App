e4e3485036e75e33b8a077da1e4039e6ae817033
edenWerke <edenw6292@gmail.com>
2026-01-17 18:34:20 +0300

    text style

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -13,10 +13,10 @@ showArrow?:boolean
 }
 
 const SettingsItem=({icon,title,onPress,textStyle,showArrow=true}:SettingItemProps )=>(
-  <TouchableOpacity>
-    <View>
-      <Image source={icon}/>
-      <Text>{title}</Text>
+  <TouchableOpacity  onPress={onPress} className='flex flex-row items-center justify-center py-3'>
+    <View className='flex flex-row items-center gap-3'>
+      <Image source={icon} className='size-6'/>
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>{title}</Text>
     </View>
   </TouchableOpacity>
 )
