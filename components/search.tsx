7f02ee2be76ec57fbe265ed32ad63cac152d4db6
edenWerke <edenw6292@gmail.com>
2026-01-18 21:41:26 +0300

    add

--- components/search.tsx
+++ components/search.tsx
@@ -1,7 +1,7 @@
 import icons from '@/constants/icons';
 import { useLocalSearchParams } from 'expo-router';
 import React, { useState } from 'react';
-import { Image, TextInput, View } from 'react-native';
+import { Image, TextInput, TouchableOpacity, View } from 'react-native';
 
 const Search = () => {
   const params = useLocalSearchParams<{ query?: string }>();
@@ -12,11 +12,18 @@ const Search = () => {
   };
 
   return (
-    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg border border-primary-100 mt-5 py-2">
+    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg border bg-accent-100 border-primary-100 mt-5 py-2">
       <View className="flex-1 flex flex-row items-center justify-start z-50">
         <Image source={icons.search} className='size-5'/>
-      <TextInput />
+      <TextInput
+      value={search}
+      onChangeText={handleSearch}
+      placeholder='Search for anything'
+      className='text-sm font-rubik text-black-300 ml-2 flex-1' />
       </View>
+      <TouchableOpacity>
+        <Image source={icons.filter} className='size-5'/>
+      </TouchableOpacity>
     </View>
   );
 };
