8bc61ffda10d5e1d7284626c5ed7b7e359d9d466
edenWerke <edenw6292@gmail.com>
2026-01-18 13:31:30 +0300

    header

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,13 +9,20 @@ export default function Index() {
      
     >
       <View className="px-5">
-    <View className="flex flex-row items-center justify-between mt-5"></View >
+    <View className="flex flex-row items-center justify-between mt-5">
      <View className="flex flex-row items-center">
-      <Image source={images.avatar} />
+      <Image source={images.avatar} className="size-12 rounded-full" />
+     
+     
+     <View className="flex flex-col items-start ml-2 justify-center">
+      <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+      <Text className="text-base font-rubik-medium text-black-300">Eden</Text>
      </View>
+     </View>
+     <Image source={icons.bell} className="size-6"/>
      
-     
-      <Text></Text>
+     </View >
+      
      </View>
     </SafeAreaView>
   );
