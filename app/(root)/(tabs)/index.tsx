a7873efa7262ec62fffe4c6a45184b6f7d33f390
edenWerke <edenw6292@gmail.com>
2026-01-19 14:20:39 +0300

    style

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import Search from "@/components/search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -26,6 +26,16 @@ export default function Index() {
       
      </View>
      <Search/>
+     <View className="my-5 ">
+      <View className="flex flex-row items-center justify-between">
+      <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+    
+    <TouchableOpacity>
+      <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+    </TouchableOpacity>
+    
+    </View>
+     </View>
     </SafeAreaView>
   );
 }
