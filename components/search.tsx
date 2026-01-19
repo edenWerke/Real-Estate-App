4995be1188a1b4bd36b802f35de3fca7fea1992c
edenWerke <edenw6292@gmail.com>
2026-01-19 14:11:53 +0300

    debounce

--- components/search.tsx
+++ components/search.tsx
@@ -1,15 +1,25 @@
 import icons from '@/constants/icons';
-import { useLocalSearchParams } from 'expo-router';
+import { router, useLocalSearchParams, usePathname, } from 'expo-router';
 import React, { useState } from 'react';
 import { Image, TextInput, TouchableOpacity, View } from 'react-native';
+import { useDebouncedCallback } from 'use-debounce';
 
 const Search = () => {
+  const path=usePathname()
   const params = useLocalSearchParams<{ query?: string }>();
   const [search, setSearch] = useState(params.query);
 
+const debounceSearch = useDebouncedCallback(
+  (text: string) => router.setParams({ query: text }),
+  500
+);
+
+
+
   const handleSearch = (text: string) => {
     setSearch(text);
-  };
+    debounceSearch(text)
+};
 
   return (
     <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg border bg-accent-100 border-primary-100 mt-5 py-2">
