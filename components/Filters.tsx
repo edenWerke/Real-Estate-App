b4f1981d1530b6b0565a9d56c22f4c2dc3e6eab3
edenWerke <edenw6292@gmail.com>
2026-01-21 22:17:21 +0300

    filtering

---
+++ components/Filters.tsx
@@ -0,0 +1,35 @@
+import { categories } from '@/constants/data';
+import { useLocalSearchParams } from 'expo-router';
+import React, { useState } from 'react';
+import { ScrollView, Text, TouchableOpacity } from 'react-native';
+
+const Filters = () => {
+  const params = useLocalSearchParams<{ filter?: string }>();
+
+  const [selectedCategory, setSelectedCategory] = useState(
+    params.filter || 'All'
+  );
+
+  const handleCategory = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      className="mt-3 mb-2"
+    >
+      {categories.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() => handleCategory(item.title)}
+        >
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default Filters;
