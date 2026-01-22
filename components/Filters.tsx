8685c01a5cb75ff3cbcc022acd6fd85658568118
edenWerke <edenw6292@gmail.com>
2026-01-22 21:41:39 +0300

    add

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,23 +1,24 @@
-import { categories } from '@/constants/data';
-import { router, useLocalSearchParams } from 'expo-router';
-import React, { useState } from 'react';
-import { ScrollView, Text, TouchableOpacity } from 'react-native';
+import { router, useLocalSearchParams } from "expo-router";
+import React, { useState } from "react";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
+
+import { categories } from "@/constants/data";
 
 const Filters = () => {
   const params = useLocalSearchParams<{ filter?: string }>();
-
   const [selectedCategory, setSelectedCategory] = useState(
-    params.filter || 'All'
+    params.filter || "All"
   );
 
-  const handleCategory = (category: string) => {
-if(selectedCategory===category){
-  setSelectedCategory('All');
-  router.setParams({filter:'All'})
-  return;
-}
-setSelectedCategory(category);
-router.setParams({filter:category})
+  const handleCategoryPress = (category: string) => {
+    if (selectedCategory === category) {
+      setSelectedCategory("");
+      router.setParams({ filter: "" });
+      return;
+    }
+
+    setSelectedCategory(category);
+    router.setParams({ filter: category });
   };
 
   return (
@@ -28,11 +29,23 @@ router.setParams({filter:category})
     >
       {categories.map((item, index) => (
         <TouchableOpacity
+          onPress={() => handleCategoryPress(item.category)}
           key={index}
-          onPress={() => handleCategory(item.title)}
-      className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory=== item.category ?'bg-primary-300':'bg-primary-100 border border-primary-200'}
- ` }>
-          <Text className={`text-sm ${selectedCategory=== item.category ?'text-white font-rubik-bold mt-0.5':'bg-primary-100 font-rubik'}`}>{item.title}</Text>
+          className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${
+            selectedCategory === item.category
+              ? "bg-primary-300"
+              : "bg-primary-100 border border-primary-200"
+          }`}
+        >
+          <Text
+            className={`text-sm ${
+              selectedCategory === item.category
+                ? "text-white font-rubik-bold mt-0.5"
+                : "text-black-300 font-rubik"
+            }`}
+          >
+            {item.title}
+          </Text>
         </TouchableOpacity>
       ))}
     </ScrollView>
