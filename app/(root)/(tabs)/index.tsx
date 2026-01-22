c5d92e646d28d4d31057286aa408e9341673757a
edenWerke <edenw6292@gmail.com>
2026-01-22 22:12:21 +0300

    global contex

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -2,11 +2,12 @@ import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
 import Search from "@/components/search";
 import icons from "@/constants/icons";
-import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const {user}=useGlobalContext()
   return (
     <SafeAreaView
      
@@ -22,12 +23,12 @@ export default function Index() {
     ListHeaderComponent={<View className="px-5">
     <View className="flex flex-row items-center justify-between mt-5">
      <View className="flex flex-row items-center">
-      <Image source={images.avatar} className="size-12 rounded-full" />
+      <Image source={{uri:user?.avatar}} className="size-12 rounded-full" />
      
      
      <View className="flex flex-col items-start ml-2 justify-center">
       <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-      <Text className="text-base font-rubik-medium text-black-300">Eden</Text>
+      <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
      </View>
      </View>
      <Image source={icons.bell} className="size-6"/>
